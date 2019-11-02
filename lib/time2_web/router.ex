95f03b61f643d7699d780b0a6caa698b6bba0e9c
jakemittlemanwebdev2019 <megamanzerox@gmail.com>
2019-11-02 19:14:53 -0400

    adding user resource

--- lib/time2_web/router.ex
+++ lib/time2_web/router.ex
@@ -9,8 +9,21 @@ defmodule Time2Web.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
+  # pipeline :api do
+  #   plug :accepts, ["json"]
+  # end
+
+  pipeline :ajax do
     plug :accepts, ["json"]
+    plug :fetch_session
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  scope "/ajax", Time2Web do
+    pipe_through :ajax
+
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   scope "/", Time2Web do
@@ -18,6 +31,7 @@ defmodule Time2Web.Router do
 
     get "/", PageController, :index
     get "/*path", PageController, :index
+    resources "/users", UserController, except: [:new, :edit]
   end
 
   # Other scopes may use custom stacks.
---
+++ lib/time2_web/views/user_view.ex
@@ -0,0 +1,20 @@
+defmodule Time2Web.UserView do
+  use Time2Web, :view
+  alias Time2Web.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      name: user.name,
+      email: user.email,
+      is_manager: user.is_manager,
+      password: user.password}
+  end
+end
